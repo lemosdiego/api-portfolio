b64a5305d69a6488df69eb2995e373ee2578cbce
Washington <washington.contato1327@gmail.com>
2025-06-20 12:19:59 -0300

    feat(database): adiciona estrutura de banco de dados ao projeto

---
+++ src/database/migrations/20250620144404-create-projects.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable("projects", {
+    id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    image_url: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    project_url: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+  });
+}
+
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable("projects");
+}
