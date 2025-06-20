196d721163d3df1f9478338d24b154f1a2094ac5
Washington <washington.contato1327@gmail.com>
2025-06-20 18:01:06 -0300

    chore(server/db): ajustes na configuração do banco e servidor

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
-import dotenv from "dotenv";
 import express from "express";
-
-dotenv.config();
+import Projects from "./models/Projects.js";
+import { Sequelize } from "sequelize";
+import config from "./config/database.js";
+import projectRoutes from "./routes/route.js";
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+const sequelize = new Sequelize(config);
+Projects.init(sequelize);
+
+app.use("/projects", projectRoutes);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("server rodando");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Erro");
+  });
