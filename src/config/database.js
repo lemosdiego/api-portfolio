d7e0a0f0d561eb07a06a32cd0dd56961a6f4b0f6
Washington <washington.contato1327@gmail.com>
2025-06-20 12:19:49 -0300

    chore(server/db): ajustes na configuração do banco e servidor

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+dotenv.config({ path: ".env.local" });
+
+export default {
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
