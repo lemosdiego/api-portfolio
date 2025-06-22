8e9a4bcce2cc12feb170f6c12439ee9c6916f56e
Washington <washington.contato1327@gmail.com>
2025-06-22 10:36:23 -0300

    Configurar dotenv e ajustar porta dinâmica para Render

--- src/server.js
+++ src/server.js
@@ -16,12 +16,14 @@ User.init(sequelize);
 app.use("/projects", projectRoutes);
 app.use("/users", userRoutes);
 
+const PORT = process.env.PORT || 3000;
+
 sequelize
   .authenticate()
   .then(() => {
     console.log("server rodando");
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
