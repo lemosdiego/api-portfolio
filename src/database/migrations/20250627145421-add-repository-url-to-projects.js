428eda8a35e8716968022d71f2ffecb8adc24932
Washington <washington.contato1327@gmail.com>
2025-06-27 13:38:14 -0300

    feat: ordena projetos por created_at DESC e adiciona migration repository_url

---
+++ src/database/migrations/20250627145421-add-repository-url-to-projects.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.addColumn("projects", "repository_url", {
+    type: Sequelize.STRING,
+    allowNull: true,
+  });
+}
+
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.removeColumn("projects", "repository_url");
+}
